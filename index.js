6f142f056ab5b8e9cad2e0ca40b5d5275dc9c062
a-blaho <blahovic.adam@icloud.com>
2023-04-24 19:29:08 +0200

    connect to DB

--- index.js
+++ index.js
@@ -42,22 +42,21 @@ client.subscribe("authenticate-user", async function ({ task, taskService }) {
     await taskService.complete(task, variables);
   } else {
     // Verify token
-    const decodedUser = await auth.verifyIdToken(token);
+    let decodedUser = undefined;
+    try {
+      decodedUser = await auth.verifyIdToken(token);
 
-    // Check if token is valid
-    if (!decodedUser) {
+      console.log("User authenticated");
+
+      variables.set("authenticated", true);
+      variables.set("user-id", decodedUser.uid);
+    } catch (error) {
       console.log("Invalid token");
 
       variables.setAll({
         authenticated: false,
         "error-message": "Invalid token",
       });
-    } else {
-      // Token is valid
-      console.log("User authenticated");
-
-      variables.set("authenticated", true);
-      variables.set("user-id", "1234");
     }
   }
 
@@ -66,7 +65,7 @@ client.subscribe("authenticate-user", async function ({ task, taskService }) {
 
 client.subscribe("retrieve-workspace", async function ({ task, taskService }) {
   console.log("Starting retrieve-workspace task");
-
+  const userId = task.variables.get("user-id");
   const workspaceId = task.variables.get("workspace-id");
 
   console.log("Got workspace ID: ", workspaceId);
@@ -82,11 +81,23 @@ client.subscribe("retrieve-workspace", async function ({ task, taskService }) {
       "error-message": "No workspace ID provided",
     });
   } else {
+    let workspaces = undefined;
     // Find workspace in firestore
-    const workspace = false;
+    try {
+      const querySnapshot = await firestore
+        .collection("workspaces")
+        .where("ownerId", "==", userId)
+        .get();
+
+      workspaces = querySnapshot.docs.map((doc) => {
+        return doc.id;
+      });
+    } catch (error) {
+      console.log(error);
+    }
 
     // Check if workspace was found
-    if (!workspace) {
+    if (!workspaces.includes(workspaceId)) {
       console.log("Workspace not found");
 
       variables.setAll({
@@ -106,6 +117,7 @@ client.subscribe("retrieve-workspace", async function ({ task, taskService }) {
 client.subscribe("retrieve-team", async function ({ task, taskService }) {
   console.log("Starting retrieve-team task");
 
+  const userId = task.variables.get("user-id");
   const teamId = task.variables.get("team-id");
 
   console.log("Got team ID: ", teamId);
@@ -122,10 +134,23 @@ client.subscribe("retrieve-team", async function ({ task, taskService }) {
     });
   } else {
     // Find team in firestore
-    const team = false;
+    let teams = undefined;
+    // Find workspace in firestore
+    try {
+      const querySnapshot = await firestore
+        .collection("teams")
+        .where("members", "array-contains", userId)
+        .get();
+
+      teams = querySnapshot.docs.map((doc) => {
+        return doc.id;
+      });
+    } catch (error) {
+      console.log(error);
+    }
 
     // Check if team was found
-    if (!team) {
+    if (!teams.includes(teamId)) {
       console.log("team not found");
 
       variables.setAll({
@@ -153,7 +178,7 @@ client.subscribe("create-task", async function ({ task, taskService }) {
   const taskDescription = task.variables.get("task-description");
   const taskColumn = task.variables.get("task-column");
 
-  const variables = new camunda.Variables();
+  const variables = new Variables();
 
   if (!taskTitle || !taskDescription || !taskColumn) {
     console.log("Missing task data");
@@ -164,11 +189,16 @@ client.subscribe("create-task", async function ({ task, taskService }) {
     });
   } else {
     // Create task in firestore
-    const taskData = false;
+    await firestore.collection("documents").doc().set({
+      title: taskTitle,
+      description: taskDescription,
+      column: taskColumn,
+      user: userId,
+    });
 
     // Task created
     console.log("Task created");
-    task.variables.setAll({
+    variables.setAll({
       taskCreated: true,
       "notification-message": "Task created",
     });
